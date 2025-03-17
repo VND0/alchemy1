3adf571360c22a152af65cc6fa7375da60b9ea70
nikita <vnd-08@yandex.ru>
2025-03-17 20:15:37 +0300

    Not working

---
+++ forms/registration.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class RegistrationForm(FlaskForm):
+    # The task says about some "Email/login" but in the DB we store emails
+    # and I have no clue where to store login according to the given schema
+    email = EmailField("Email", validators=[DataRequired()])
+    passwd = PasswordField("Password", validators=[DataRequired()])
+    passwd_confirmation = PasswordField("Repeat password", validators=[DataRequired()])
+    surname = StringField("Surname",validators=[ DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    age = IntegerField("Age", validators=[DataRequired()])
+    position = StringField("Position", validators=[DataRequired()])
+    speciality = StringField("Speciality", validators=[DataRequired()])
+    address = StringField("Address", validators=[DataRequired()])
+    submit = SubmitField("Submit")
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from datetime import datetime as dt, timedelta as td
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash
 
 from data import db_session
+from data.db_session import create_session
 from data.job import Job
 from data.users import User
+from forms.registration import RegistrationForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "89d5be8d17a5422d76807e1f3f53b2d3"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 
 def add_people():
     session = db_session.create_session()
@@ -39,6 +43,42 @@ def add_jobs():
     ]
     session.add_all(jobs)
     session.commit()
+
+
+def handle_registration(form: RegistrationForm) -> str | None:
+    try:
+        assert form.passwd == form.passwd_confirmation
+
+        user = User(
+            surname=form.surname,
+            name=form.name,
+            age=form.age,
+            position=form.position,
+            speciality=form.speciality,
+            address=form.address,
+            email=form.email,
+            hashed_password=generate_password_hash(form.passwd)
+        )
+        session = create_session()
+        session.add(user)
+        session.commit()
+    except AssertionError:
+        return "Passwords are not equal"
+    except IntegrityError:
+        return "User already exists or some data is mailformed"
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    err = None
+    form = RegistrationForm()
+    if form.validate_on_submit():  # Почему тут в иф не заходит?
+        err = handle_registration(form)
+        print(err)
+        if err is None:
+            return redirect("index")
+
+    return render_template("register.html", title="Регистрация", form=form, err=err)
 
 
 @app.get("/")
