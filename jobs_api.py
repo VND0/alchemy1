bc342662a92eb0c619b1c2cff409e6df5965eff4
nikita <vnd-08@yandex.ru>
2025-03-23 17:55:08 +0300

    Added route to get certain job by id

--- jobs_api.py
+++ jobs_api.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, jsonify
+from typing import Any
+
+from flask import Blueprint, jsonify, Response, make_response
+from sqlalchemy.exc import NoResultFound
+
 from data import db_session
 from data.job import Job
 
@@ -10,8 +14,25 @@ bp = Blueprint(
 )
 
 
+def form_error(message: Any, status_code: int = 400) -> Response:
+    return make_response({"status": "error", "message": message}, status_code)
+
+
 @bp.get("/jobs")
 def get_jobs():
     session = db_session.create_session()
     jobs = session.query(Job).all()
     return jsonify([j.serialize() for j in jobs])
+
+
+@bp.get("/jobs/<job_id>")
+def get_job(job_id):
+    session = db_session.create_session()
+    try:
+        job = session.query(Job).filter(Job.id == int(job_id)).one()
+    except ValueError:
+        return form_error("Id is not an integer")
+    except NoResultFound:
+        return form_error("Couldn't find job with this id")
+
+    return jsonify(job.serialize())
