6f1f59fbeaa9ce475c4d36def5b364a379120704
nikita <vnd-08@yandex.ru>
2025-03-25 19:37:28 +0300

    Added test cases for deletion.

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -3,6 +3,7 @@ import pprint
 import requests
 
 import test_create
+import test_get
 
 URL = "http://localhost:8080/api/jobs"
 
@@ -17,10 +18,13 @@ def add_valid_and_get_id() -> int:
 
 def valid_deletion():
     job_id = add_valid_and_get_id()
+
+    test_get.all_jobs()
     print("DELETING BY VALID ID")
     response = requests.delete(URL + f"/{job_id}")
     print(f"STATUS CODE {response.status_code}")
     pprint.pp(response.json())
+    test_get.all_jobs()
 
 
 def bad_id():
@@ -30,6 +34,14 @@ def bad_id():
     pprint.pp(response.json())
 
 
+def bad_datatype():
+    print("DELETING BY BAD ID string")
+    response = requests.delete(URL + f"/string")
+    print(f"STATUS CODE {response.status_code}")
+    pprint.pp(response.json())
+
+
 if __name__ == '__main__':
     valid_deletion()
     bad_id()
+    bad_datatype()
